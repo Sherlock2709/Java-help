4e709f553a3621327fa084387f20f6405769c90a
Pranjal Mehrotra <pranjalmehrotra1996@gmail.com>
2019-01-31 10:53:42 +0530

    Question
    Create a class named Player with the following private member variables/attributes 
    String name 
    String country 
    String skill 
    
    Include appropriate getters and setters. 
    [Naming Convention: 
    getters : getName getCountry... 
    setters : setName setCountry...] 
    
    Create another class and write the main method to test the above class. In the main method, get the choice from the user and update the corresponding player details. 
    
     Input and Output Format: 
     Refer sample input and output for formatting specifications. 
    All text in bold corresponds to the input and the rest corresponds to output. 
    
     Sample Input and Output : 
    Enter the player name 
    MS Dhoni 
    Enter the country name 
    India 
    Enter the skill 
    All Rounder 
    Player Details 
    Player Name :MS Dhoni 
    Country Name :India 
    Skill :All Rounder 
    Verify and Update Player Details 
    Menu 
    1.Update Player Name 
    2.Update Country Name 
    3.Update Skill 
    4.All informations Correct/Exit 
    Type 1 or 2 or 3 or 4 
    1 
    The current player name is MS Dhoni 
    Enter the player name 
    Albie Morkel 
    Menu 
    1.Update Player Name 
    2.Update Country Name 
    3.Update Skill 
    4.All informations Correct/Exit 
    Type 1 or 2 or 3 or 4 
    4 
    Player Details 
    Player Name :Albie Morkel 
    Country Name :India 
    Skill :All Rounder

---
+++ Player.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Player
+{
+    private String name,country, skill;
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String newname)
+    {
+        this.name = newname;
+    }
+    public String getCountry()
+    {
+        return country;
+    }
+    public void setCountry(String newcountry)
+    {
+        this.country = newcountry;
+    }
+    public String getSkill()
+    {
+        return skill;
+    }
+    public void setSkill(String newskill)
+    {
+        this.skill = newskill;
+    }
+}
